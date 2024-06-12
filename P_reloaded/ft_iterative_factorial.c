a901632ee5d79ec41f70df5e7462d53fe05ebf71
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2024-06-13 01:23:23 +0200

    added ex12

---
+++ P_reloaded/ft_iterative_factorial.c
@@ -0,0 +1,19 @@
+int ft_iterative_favtorial(int nb)
+{
+    int r;
+
+    r = nb;
+    if (r < 0 || r > 15)
+        return (0);
+    else if (r == 0 || r == 1)
+        return (1);
+    else
+    {
+        while (r > 1)
+        {
+            r--;
+            nb *= r;
+        }
+    }
+    return (nb);
+}
