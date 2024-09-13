f4f8558d0cd37196a12d7b1c6dc4725e374037d4
JMedina9741 <147419480+JMedina9741@users.noreply.github.com>
2024-09-13 21:25:31 +0200

    modAndTry

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: javmedin <javmedin@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 21:19:58 by javmedin          #+#    #+#             */
-/*   Updated: 2024/09/13 21:20:46 by javmedin         ###   ########.fr       */
+/*   Updated: 2024/09/13 21:24:26 by javmedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int				ft_putnbr(int n);
 int				ft_putstr(char *s);
 int				ft_printf(const	char *str, ...);
 unsigned int	ft_uns_num(unsigned int num);
-int				ft_punt_hexa(void *p);
+int				ft_point_hexa(void *p);
 size_t			ft_strlen(const	char *s);
 int				ft_hexa_mincap(int num);
 int				ft_hexa_cap(int num);
